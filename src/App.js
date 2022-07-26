79002e8c368675f707499618007dad0088f0caab
Piotr Filipiak <kiddhkane@gmail.com>
2022-07-26 23:03:50 +0200

    Fetching tours.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Loading from "./Loading";
 import Tours from "./Tours";
 
@@ -7,6 +7,18 @@ const url = "https://course-api.com/react-tours-project";
 function App() {
   const [loading, setLoading] = useState(true);
   const [tours, setTours] = useState([]);
+
+  const fetchTours = async () => {
+    setLoading(true);
+    const response = await fetch(url);
+    const tours = await response.json();
+    console.log(tours);
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
   if (loading) {
     return (
       <main>
@@ -14,7 +26,11 @@ function App() {
       </main>
     );
   }
-  return <h2>Main app</h2>;
+  return (
+    <main>
+      <Tours />
+    </main>
+  );
 }
 
 export default App;
