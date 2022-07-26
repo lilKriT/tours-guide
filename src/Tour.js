d4f101064ac226d1806bfa1bb1c4cae043de4a62
Piotr Filipiak <kiddhkane@gmail.com>
2022-07-27 00:43:23 +0200

    Listing tours.

--- src/Tour.js
+++ src/Tour.js
@@ -1,7 +1,19 @@
-import React from "react";
+import { useState } from "react";
 
-const Tour = () => {
-  return <div>Tour</div>;
+const Tour = ({ id, image, info, price, name }) => {
+  return (
+    <article className="single-tour">
+      <img src={image} alt={name} />
+      <footer>
+        <div className="tour-info">
+          <h4>{name}</h4>
+          <h4 className="tour-price">${price}</h4>
+        </div>
+        <p>{info}</p>
+        <button className="delete-btn">Not interested</button>
+      </footer>
+    </article>
+  );
 };
 
 export default Tour;
